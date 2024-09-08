c59c8d850a95df6f6252aa0f4fad2f3add242121
Ejim Chisom <136533001+ej1m@users.noreply.github.com>
2024-09-08 20:30:29 +0200

    Add files via upload

---
+++ exploratory_layoffs.sql
@@ -0,0 +1,71 @@
+SELECT *
+FROM layoffs_staging2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY total_laid_off DESC;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+SELECT company, SUM(total_laid_off),MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)  # date range within which layoffs occured
+FROM layoffs_staging2;
+
+SELECT industry, SUM(total_laid_off),MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+SELECT country, SUM(total_laid_off),MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)  # date range within which layoffs occured
+FROM layoffs_staging2;
+
+SELECT YEAR(`date`), SUM(total_laid_off),MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 2 DESC;
+
+SELECT stage, SUM(total_laid_off),MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+WITH Rolling_Total AS (
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1
+)
+SELECT `MONTH`, total_off,
+SUM(total_off) OVER (ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+WITH company_year (company, years,total_laid_off) AS (
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), company_year_rank AS (
+SELECT *, 
+DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM company_year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM company_year_rank
+WHERE Ranking < 6;
